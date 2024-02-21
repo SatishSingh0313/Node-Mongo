37a3831faa025261e3170c7b5e9181c90a5d3a84
SatishSingh0313 <satishsangam.205@gmail.com>
2024-02-22 01:24:06 +0530

    add db

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app=express();
 app.get('/',(req,resp)=>{
    
     resp.send("Hello Node Js")
-    resp.send("Hello Node Mongo")
+    resp.send("Hello Node Mongo  (DB)")
 })
 app.listen(4000);
