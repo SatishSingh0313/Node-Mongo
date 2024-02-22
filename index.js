2f89f8327c19578323dbf77a23600b85f821a2c6
SatishSingh0313 <satishsangam.205@gmail.com>
2024-02-22 23:30:33 +0530

    Index file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-const InsertDB=async ()=>{
+const Insert=async ()=>{
     await mongoose.connect('mongodb://127.0.0.1:27017/e-com');
 
     const ProductSchema=new mongoose.Schema({
@@ -14,6 +14,6 @@ const InsertDB=async ()=>{
     let result=await data.save();
     console.log(result);
 }
-InsertDB();
+Insert();
 
 
